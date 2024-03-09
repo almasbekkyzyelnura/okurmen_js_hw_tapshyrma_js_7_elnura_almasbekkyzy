e610fd6d6eb4d31de82cf710265450f3246a8572
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-10 00:52:00 +0600

    tapshyrmajs 7

---
+++ index.js
@@ -0,0 +1,125 @@
+/* function numbers() {
+    //     let data = [];
+    //     for (let i = 0; i <= 100; i++) {
+    //         if (i % 2 === 0) {
+    //             data.push(i);
+    //         }
+    //     }
+    //     return data;
+    // }
+    
+    // let dataArray = numbers();
+    // console.log(dataArray);
+    
+    
+    
+    
+    
+    // const numbers = [1.456, 2.125, 3.32, 4.1, 5.34];
+        
+    //     console.log(Math.ceil(1.456));
+    //     console.log(Math.ceil(2.125));
+    //     console.log(Math.ceil(3.32));
+    //     console.log(Math.ceil(4.1));
+    //     console.log(Math.ceil(5.34));
+    
+    */
+    
+    
+    
+        /*
+        
+        let date = new Date('2024-03-01');
+        let day = date.getDate ="01";
+        let month = date.getMonth ="03" ;
+        let year = date.getFullYear('2024');
+
+        let formatteredDate = day + "-" + month + "-" + year ;
+        console.log(formatteredDate);
+        console.log("year" +  day , "month" + month , "day" + year);
+    
+    */
+    
+    
+    
+        /*
+        function createString(array) {
+            let result = '';
+             for (let i = 0; i < array.length; i++) {
+                 if (i < array.length - 1) {
+                     result += array[i] + '  ';
+                 } else {
+                    result += array[i];
+                }
+             }
+             return result;
+         }
+        
+         let arr = [3, 2, 1];
+         let resultString = createString(arr);
+         console.log(resultString);
+
+        console.log(arr.join('\n-'));
+        const newA = arr.join(arr);
+        */
+    
+    
+    
+    
+    
+    
+    // let start = '';
+    // for (let i = 1; i <=7; i++){
+    //     start += '*';
+    //     console.log(start);
+    // }
+    
+    
+    
+    
+    
+    
+      
+    //    let obj = {
+        //     a: 11,
+        //     b: 12,
+        //     c: 13,
+        //     d: 14,
+        //     e: 15,
+        //     f: 16,
+        //     g: 17,
+        //     h: 18,
+        //   };
+          
+        //   let sum = 0;
+        //   for (let key in obj) {
+        //     sum += obj[key];
+        //   }
+          
+        //   console.log(sum);
+    
+    
+    
+    
+    
+    
+    
+    // const randomBodyParts = ['акылдуу','сулуу','ыймандуу'];
+    // const randomAdjectives = ['эже','бала','кыз'];
+    // const randomWords = ['жакшынакайсын','татынакайсын','суйкумдуусун',];
+    
+    // function getRandomWord(array) {
+    //     const word = array[Math.floor(Math.random() * array.length)];
+    
+    //     return word;
+    // };
+    
+    // const text = `сен ${getRandomWord
+    //     (randomBodyParts)}  кызсын ${getRandomWord(randomAdjectives)} жана ${getRandomWord(
+    //         randomWords
+    
+    //     )}!!!`; 
+    
+    //     console.log(text);
+    
+    
